783292a4100acf23f2a3d05a793133c118ccd4d5
Wellington.Nodari <10558156@mydbs.ie>
2022-05-12 21:59:50 +0100

    Buttons and Forms - adding email

--- myApp.py
+++ myApp.py
@@ -10,22 +10,25 @@ app = Flask(__name__)
 def hello():
     return render_template("/html/index.html")
 
-database = {'well': '123', 'tha': 'qwert', 'neusa': 'bere'}
+database = {'well@pp.ie': '123', 'tha': 'qwert', 'neusa': 'bere'}
 
 
 @app.route('/form_login', methods=['POST', 'GET'])
 def login():
-    name1 = request.form['username']
+    email = request.form['email']
     pwd = request.form['password']
 
-    if name1 not in database:
-        return render_template('/html/index.html', info='Invalid User')
+    if email is not None:
+        if email not in database:
+            return render_template('/html/index.html', info='Invalid Email')
 
-    else:
-        if database[name1] != pwd:
-            return render_template('/html/index.html', info='Invalid Password')
         else:
-            return render_template('/html/booking/booking.html', name=name1)
+            if database[email] != pwd:
+                return render_template('/html/index.html', info='Invalid Password')
+            else:
+                return render_template('/html/booking/booking.html', name=request.form['username'])
+    else:
+        return render_template('/html/index.html', info='')
 
 @app.route('/form_signup', methods=['POST', 'GET'])
 def signup():
