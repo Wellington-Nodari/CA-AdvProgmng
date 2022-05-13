599e1b4ad749cef49e89f2e4095784c5d35ee048
Wellington.Nodari <10558156@mydbs.ie>
2022-05-13 16:07:01 +0100

    deploying DB - test 10

--- myApp.py
+++ myApp.py
@@ -1,76 +1,71 @@
 from flask import Flask, request, render_template
-###from flask_mysqldb import MySQL
+from flask_mysqldb import MySQL
 from flask_cors import CORS
 import mysql.connector
 import json
 
 app = Flask(__name__)
-###mysql = MySQL(app)
+mysql = MySQL()
 CORS(app)
-mysql = mysql.connector.connect(
-    host='localhost',
-    user='root',
-    password='P21m38f77*',
-    port='3306',
-    database='mycadbs'
-)
-mycursor = mysql.cursor()
-mycursor.execute('SELECT * FROM login;')
-users = mycursor.fetchall()
 
-####
-# app.config['MYSQL_USER'] = 'root'
-# app.config['MYSQL_PASSWORD'] = 'P21m38f77*'
-# app.config['MYSQL_DB'] = 'mycadbs'
-# app.config['MYSQL_HOST'] = 'localhost' #for now
-# mysql.init_app(app)
-####
+
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'P21m38f77*'
+app.config['MYSQL_DB'] = 'mycadbs'
+app.config['MYSQL_HOST'] = 'localhost' #for now
+mysql.init_app(app)
+
 
 @app.route('/')
 def hello():
-    return print(users) #results[0]['name'] #render_template("/html/index.html")
+    return render_template("/html/index.html")
 
 # database = {'well@pp.ie': '123', 'tha': 'qwert', 'neusa': 'bere'}
 
 
-# @app.route('/form_login', methods=['POST', 'GET'])
-# def login():
-#     #username = request.form['username']
-#     email = request.form['email']
-#     pwd = request.form['password']
-#
-#     if email is not None:
-#         if email not in database:
-#             return render_template('/html/index.html', info='Invalid Email')
-#
-#         else:
-#             if database[email] != pwd:
-#                 return render_template('/html/index.html', info='Invalid Password')
-#             else:
-#                 return render_template('/html/booking/booking.html', name=email) #request.form['username'])
-#     else:
-#         return render_template('/html/index.html', info='')
-#
-# @app.route('/form_signup', methods=['POST', 'GET'])
-# def signup():
-#     name1 = request.form['username']
-#     email = request.form['email']
-#     pwd = request.form['password']
-#     cpwd = request.form['password']
-#
-#     if name1 in database:
-#         return render_template('/html/index.html', info='User name already in use. Choose another one.')
-#     if email in database:
-#         return render_template('/html/index.html', info='Email already in use. Choose another one.')
-#     if pwd != cpwd:
-#         return render_template('/html/index.html', info='The passwords must match.')
-#
-#     else:
-#         if database[email] != pwd:
-#             return render_template('/html/index.html', info='Invalid Password')
-#         else:
-#             return render_template('/html/booking/booking.html', name=name1)
-#
+@app.route('/form_login', methods=['POST', 'GET'])
+def login():
+    #username = request.form['username']
+    email = request.form['email']
+    pwd = request.form['password']
+
+    if email is not None:
+        if email not in mysql:
+            return render_template('/html/index.html', info='Invalid Email')
+
+        else:
+            if mysql[email] != pwd:
+                return render_template('/html/index.html', info='Invalid Password')
+            else:
+                return render_template('/html/booking/booking.html', name=email) #request.form['username'])
+    else:
+        return render_template('/html/index.html', info='')
+
+@app.route('/form_signup', methods=['POST', 'GET'])
+def signup():
+    name = request.args.get('username')
+    email = request.args.get('email')
+    pwd = request.args.get('pwd')
+    pwdC = request.args.get['pwd']
+
+    cur = mysql.connection.cursor()
+    s = '''INSERT INTO login(username, email, pwd) VALUES('{}','{}','{}');'''.format(name, email, pwd)
+    cur.execute(s)
+    mysql.connection.commit()
+
+    if name in mysql:
+        return render_template('/html/index.html', info='User name already in use. Choose another one.')
+    if email in mysql:
+        return render_template('/html/index.html', info='Email already in use. Choose another one.')
+    if pwdC != pwd:
+        return render_template('/html/index.html', info='The passwords must match.')
+
+    else:
+        if mysql[email] != pwd:
+            return render_template('/html/index.html', info='Invalid Password')
+        else:
+            return render_template('/html/booking/booking.html', name=name)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
