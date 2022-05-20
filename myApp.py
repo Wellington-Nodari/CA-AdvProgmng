be011c00be7d7cb8e0af498364e86db1912ca7df
Wellington.Nodari <10558156@mydbs.ie>
2022-05-20 16:43:51 +0100

    adjusting the new tables on DB

--- myApp.py
+++ myApp.py
@@ -1,11 +1,13 @@
 from flask import Flask, request, render_template, redirect, url_for, session, flash, g
 from functools import wraps
 import sqlite3
+import datetime
 import json
 
 app = Flask(__name__)
 app.secret_key = "diamond" #check this!!!
 app.database = "myschool.db"
+now = datetime.datetime.now()
 
 @app.route('/')
 def home():
@@ -27,7 +29,7 @@ def main():
 
     g.db = connect_db()
     cur = g.db.execute('select * from students')
-    students = [dict(studentId=row[0], username=row[1], email=row[2]) for row in cur.fetchall()]
+    students = [dict(studentId=row[0], fname=row[1], lname=row[2], email=row[3]) for row in cur.fetchall()]
     g.db.close()
     return render_template('/html/booking/booking.html', students=students)
 
@@ -41,14 +43,14 @@ def login():
     email = str(request.form["email"])
     pwd = str(request.form["password"])
     g.db = connect_db()
-    cur = g.db.execute('select * from students')
-    st = f"SELECT * FROM students WHERE email='{email}' AND password='{pwd}';"
+    cur = g.db.execute('select * from loginCred')
+    st = f"SELECT * FROM loginCred WHERE email='{email}' AND password='{pwd}';"
     cur.execute(st)
     e = cur.fetchall()
     # nameLogin = e[0][1]
-    # print(nameLogin)
+    print(e)
     try:
-        if e[0][2] == email and e[0][3] == pwd:
+        if e[0][1] == email and e[0][2] == pwd:
             session['logged_in'] = True
             g.db.close()
             flash("You were just logged in!")
@@ -67,14 +69,18 @@ def connect_db():
 
 @app.route('/enroll', methods=['POST', 'GET'])
 def enroll():
-    name = str(request.form['username'])
+    fname = str(request.form['fname'])
+    lname = str(request.form['lname'])
     email = str(request.form['email'])
+    course = str(request.form['course'])
     pwd = str(request.form['password'])
     pwdc = str(request.form['pwdC'])
 
     g.db = connect_db()
-    cur = g.db.execute("INSERT INTO students (username, email, password) VALUES ('{}','{}','{}');".format(name, email, pwd))
-    add = cur.fetchall()
+    cur = g.db.execute("INSERT INTO students (fname, lname, email) VALUES ('{}','{}','{}');".format(fname, lname, email))
+    cur1 = g.db.execute("INSERT INTO loginCred (email, password) VALUES ('{}','{}');".format(email, pwd))
+    cur2 = g.db.execute("INSERT INTO std_enroll (email, courseName, enrlDate) VALUES ('{}','{}','{}');".format(email, course, now))
+    add = cur.fetchall(), cur1.fetchall(), cur2.fetchall()
 
     if pwd != pwdc:
         return render_template('/html/index.html', error='The passwords much match.')
--- sql.py
+++ sql.py
@@ -2,25 +2,26 @@ import sqlite3
 
 with sqlite3.connect("myschool.db") as connection:
     c = connection.cursor()
-    #c.execute('DROP TABLE students')
-    #c.execute("""CREATE TABLE students(studentId INTEGER PRIMARY KEY AUTOINCREMENT,username VARCHAR(50),email VARCHAR(50),password VARCHAR(50))""")
+    # c.execute('DROP TABLE room')
+    # c.execute("""CREATE TABLE students(studentId INTEGER PRIMARY KEY AUTOINCREMENT,username VARCHAR(50),email VARCHAR(50),password VARCHAR(50))""")
     # c.execute("""CREATE TABLE students(studentId INTEGER PRIMARY KEY AUTOINCREMENT,fname VARCHAR(50),lname VARCHAR(50),email VARCHAR(50))""")
     # c.execute("""CREATE TABLE courses(courseId INTEGER PRIMARY KEY AUTOINCREMENT,courseName VARCHAR(50),duration INTEGER,price REAL)""")
-    # c.execute("""CREATE TABLE std_enroll(enrollId INTEGER PRIMARY KEY AUTOINCREMENT,studentId INTEGER,courseId INTEGER,enrlDate TEXT, FOREIGN KEY(studentId) REFERENCES students(studentId), FOREIGN KEY(courseId) REFERENCES courses(courseId))""")
+    # c.execute("""CREATE TABLE std_enroll(enrollId INTEGER PRIMARY KEY AUTOINCREMENT,studentId INTEGER,courseId INTEGER, courseName VARCHAR(50),enrlDate TEXT, FOREIGN KEY(studentId) REFERENCES students(studentId), FOREIGN KEY(courseName) REFERENCES courses(courseName))""")
+    #c.execute("""CREATE TABLE loginCred(credentialId INTEGER PRIMARY KEY AUTOINCREMENT,email VARCHAR(50),password VARCHAR(50), FOREIGN KEY(email) REFERENCES students(email))""")
 
-    c.execute('INSERT INTO courses (courseName, duration, price) VALUES("Data Science", "24","950.00")')
-    c.execute('INSERT INTO courses (courseName, duration, price) VALUES("Software Development", "24","1000.00")')
-    c.execute('INSERT INTO courses (courseName, duration, price) VALUES("DevOps", "12","850.00")')
-    c.execute('INSERT INTO courses (courseName, duration, price) VALUES("UI/UX Design", "12","650.00")')
-    c.execute('INSERT INTO courses (courseName, duration, price) VALUES("Blockchain", "12","1150.00")')
-    c.execute('INSERT INTO courses (courseName, duration, price) VALUES("Cybersecurity", "24","850.00")')
+    # c.execute('INSERT INTO courses (courseName, duration, price) VALUES("Data Science", "24","950.00")')
+    # c.execute('INSERT INTO courses (courseName, duration, price) VALUES("Software Development", "24","1000.00")')
+    # c.execute('INSERT INTO courses (courseName, duration, price) VALUES("DevOps", "12","850.00")')
+    # c.execute('INSERT INTO courses (courseName, duration, price) VALUES("UI/UX Design", "12","650.00")')
+    # c.execute('INSERT INTO courses (courseName, duration, price) VALUES("Blockchain", "12","1150.00")')
+    # c.execute('INSERT INTO courses (courseName, duration, price) VALUES("Cybersecurity", "24","850.00")')
     #c.execute('INSERT INTO students (username, email, password) VALUES("Tha", "w","321")')
     #c.execute('INSERT INTO students (username, email, password) VALUES("dog", "d@nod.ie","987")')
 
 
 
-    # c.execute('INSERT INTO room (roomName, maxGuests) VALUES("Sea Breeze", 2)')
-    # c.execute('INSERT INTO room (roomName, maxGuests) VALUES("Sun Shine", 3)')
-    # c.execute('INSERT INTO room (roomName, maxGuests) VALUES("Tropical Mood", 4)')
+    # c.execute('INSERT INTO students (fname, lname, email) VALUES("John", "Wick", "jw@mail.com")')
+    # c.execute('INSERT INTO std_enroll (courseName, enrlDate) VALUES("DevOps", "2022-05-01")')
+    # c.execute('INSERT INTO loginCred (email, password) VALUES("jw@mail.com", "123")')
     # c.execute('INSERT INTO room (roomName, maxGuests) VALUES("Blue Moon", 2)')
 
